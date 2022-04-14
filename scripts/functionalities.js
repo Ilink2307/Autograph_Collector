bb875f71425ac9ede991377067456865f17b11ea
Ilinca Dumitrascu <ilinca.dumitrascu@gmail.com>
2022-04-14 20:11:10 +0300

    Autograph dropdown

---
+++ scripts/functionalities.js
@@ -0,0 +1,9 @@
+document.getElementById("autographButton").setAttribute("onclick", "myFunction()");
+
+function myFunction() {
+    document.getElementById("autographDetails").classList.toggle("show");
+}
+
+function test(){
+    document.getElementById("demo").style.color="red";
+}
