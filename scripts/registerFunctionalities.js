35030998bec1d68ec379bcd80703427d3b1b3338
Delia-Ioana Oloeriu <deliaoloeriu18@gmail.com>
2022-05-27 01:11:06 +0300

    started to integrate api

--- scripts/registerFunctionalities.js
+++ scripts/registerFunctionalities.js
@@ -1,4 +1,28 @@
 function validateFormOnSubmit(RegisterForm){
     return RegisterForm != null;
+}
+
+function registerUser() {
+    let email = document.getElementById("email").value;
+    let username = document.getElementById("userName").value;
+    let password = document.getElementById("password").value;
+
+    let params = email+'/'+username+'/'+password;
+
+    let url = 'http://localhost:8081/api/register/' + params;
+    alert(url);
+
+    let httpRequest = new XMLHttpRequest();
+    if (!httpRequest) {
+        alert(' Cannot create an XMLHTTP instance');
+        return false;
+    }
+
+    httpRequest.open('POST', url);
+
+    httpRequest.onload = () => console.log(httpRequest.responseText);
+
+    httpRequest.send();
+
 
 }
